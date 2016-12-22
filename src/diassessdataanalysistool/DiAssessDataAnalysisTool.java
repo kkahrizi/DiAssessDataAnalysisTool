84e37f5a993fc0846f4995b1adca01f2121609c2
Kamin Kahrizi <kamin@diassess.com>
2016-12-22 07:28:02 -0800

    first commit

---
+++ src/diassessdataanalysistool/DiAssessDataAnalysisTool.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diassessdataanalysistool;
+import ToolUI.DiAssessDataAnalysisToolUI;
+import javax.swing.JFrame;
+/**
+ *
+ * @author kamin
+ */
+public class DiAssessDataAnalysisTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame myFrame = new DiAssessDataAnalysisToolUI();
+        myFrame.setVisible(true);
+    }
+    
+}
